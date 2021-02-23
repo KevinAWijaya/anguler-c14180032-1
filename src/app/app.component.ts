42e317abd42eb434d137a6de27bc1c7d7ba79427
KevinAWijaya <kevinaw46@gmail.com>
2021-02-23 18:11:40 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  a = 12;
+  b = 21;
+
+  item = [
+    {nama : "Jeruk", harga : 15000, satuan : "kg"},
+    {nama : "Apel", harga : 2000, satuan : "kg"}
+  ]
+
+  tampil : boolean = true;
+  tulisanopen : String;
+
+  OPEN(){
+    this.tulisanopen="BUTTON CLICKED";
+    alert("clicked");
+  }
+
+  values = "";
+  onKey(event : any){
+    this.values += event.target.value + '|'
+  }
+
 }
